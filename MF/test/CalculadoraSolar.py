adf984ff3b9c5a2d2552ed41e887652880e70afc
FelipeBMN <felipebrunomn@gmail.com>
2021-08-30 09:44:23 -0300

    ok

--- MF/test/CalculadoraSolar.py
+++ MF/test/CalculadoraSolar.py
@@ -9,11 +9,13 @@ from kivymd.uix.toolbar import MDToolbar
 
 class CalculadoraSolar(MDApp):
     def build(self):
+        self.state = 0 #initial state
+        #self.theme_cls.primary_palette = "DeepOrange"
         screen = MDScreen()
-        self.toobar = MDToolbar(title="CalculadoraSolar")
-        self.toobar.pos_hint = {"top": 1}
+        self.toobar = MDToolbar()
+        self.toobar.pos_hint = {"top": 1.05}
         screen.add_widget(self.toobar)
         return screen
 
 if __name__ == '__main__':
-    CalculadoraSolar().run
+    CalculadoraSolar().run()

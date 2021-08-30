a0f034087691e5be5f15f283e24f76843e2870e9
FelipeBMN <felipebrunomn@gmail.com>
2021-08-30 00:05:19 -0300

    ok

--- MF/test/CalculadoraSolar.py
+++ MF/test/CalculadoraSolar.py
@@ -1,2 +1,16 @@
-import math
+from kivymd.uix.screen import MDScreen
+from kivymd.app import MDApp
+from kivy.uix.image import Image
+from kivy.uix.button import MDFillRoundFlatIconButton, MDFillRoundFlatButton
+from kivy.uix.textfield import MDFTextField
+from kivy.uix.label import MDFLabel
+from kivy.uix.toobar import MDToolbar
 
+class CalculadoraSolar(MDApp):
+    def build(self):
+        screen = MDScreen()
+
+        MDToolbar(title="")
+        return screen
+if __name__ == '__main__':
+    CalculadoraSolar().run

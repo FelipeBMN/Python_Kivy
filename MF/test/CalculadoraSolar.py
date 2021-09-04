fe783ea9afcfa38158629dbdd3192ccf1fd576c2
FelipeBMN <felipebrunomn@gmail.com>
2021-09-04 11:26:19 -0300

    ok

--- MF/test/CalculadoraSolar.py
+++ MF/test/CalculadoraSolar.py
@@ -13,17 +13,29 @@ from kivymd.uix.snackbar import Snackbar
 
 
 class CalculadoraSolar(MDApp):
-    def callback(self, button):
-                self.menu.caller = button
-                self.menu.open()
+    user = False
+    def menu(self, button):
+                self.menu_principal.caller = button
+                self.menu_principal.open()
 
-    def user(self, button):
-                self.menu_user.caller = button
-                self.menu_user.open()
+    def menu_user(self,button):
+        if self.user == False :
+            self.user_deslogado(button)
+        if self.user == True :
+            print("User Logado!!!")
 
-    def menu_callback_user(self, text_item):        
-        self.menu.dismiss()
+
+    def user_deslogado(self, button):
+                self.menu_user_deslogado.caller = button
+                self.menu_user_deslogado.open()
+
+    def menu_callback(self, text_item):        
+        self.menu_principal.dismiss()
         Snackbar(text=text_item).open()
+
+    def logar(self, text_item_user):        
+        self.menu_user_deslogado.dismiss()
+        Snackbar(text=text_item_user).open()
 
     def build(self):
         self.state = 0 #initial state
@@ -33,8 +45,8 @@ class CalculadoraSolar(MDApp):
         self.toobar = MDToolbar()
         self.toobar.title = 'Diario de Obra'
         self.toobar.pos_hint = {"top": 1}        
-        self.toobar.left_action_items = [["menu", lambda x: self.callback(x)]]
-        self.toobar.right_action_items = [["account-circle", lambda x: self.user(x)]]
+        self.toobar.left_action_items = [["menu", lambda x: self.menu(x)]]
+        self.toobar.right_action_items = [["account-circle", lambda x: self.menu_user(x)]]
         screen.add_widget(self.toobar)
         
 
@@ -43,6 +55,7 @@ class CalculadoraSolar(MDApp):
         #    pos_hint = {"center_x":0.5,"center_y":0.95}
         #))
 
+        # Menu Principal
         menu_items = [
             {
                 "viewclass": "OneLineListItem",
@@ -51,23 +64,22 @@ class CalculadoraSolar(MDApp):
                 "on_release": lambda x=f"Item {i}": self.menu_callback(x),
              } for i in range(5)
         ]
-
-        menu_items_user = [
+        self.menu_principal = MDDropdownMenu(
+            items = menu_items,
+            width_mult = 4,
+        )
+        
+        # Menu do Usuário
+        menu_items_user_deslogado = [
             {
                 "viewclass": "OneLineListItem",
                 "text": "Logar",
                 "height": dp(40),
-                "on_release": lambda x="Login": self.menu_callback_user(x),
+                "on_release": lambda x="Login": self.logar(x),
              } 
         ]
-
-        self.menu = MDDropdownMenu(
-            items = menu_items,
-            width_mult = 4,
-        )
-
-        self.menu_user = MDDropdownMenu(
-            items = menu_items_user,
+        self.menu_user_deslogado = MDDropdownMenu(
+            items = menu_items_user_deslogado,
             width_mult = 2,
         )
         

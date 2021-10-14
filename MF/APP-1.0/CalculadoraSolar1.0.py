005c686c84be5057816a1e313217e05530a974f5
FelipeBMN <felipebrunomn@gmail.com>
2021-10-14 11:54:18 -0300

    felipe

--- MF/APP-1.0/CalculadoraSolar1.0.py
+++ MF/APP-1.0/CalculadoraSolar1.0.py
@@ -14,115 +14,133 @@ from kivymd.uix.snackbar import Snackbar
 
 class CalculadoraSolar(MDApp):
     user = False
-    
+    calculado = 0
+    screen = MDScreen()
+    # Função para o botão Calcular
     def calcular(self, args):
-        self.detalhes.text="121,5"
+        if self.calculado == 0:
+            self.screen.remove_widget(self.input_municipio)
+            self.screen.remove_widget(self.input_placas)
+            self.screen.remove_widget(self.input_pot_placas)
+        self.calculado = 1
         return 0
 
-    def flip(self):
-        # a function for the "flip" icon
-        # changes the state of the app
-        if self.state == 0:
-            self.state = 1
-            self.toolbar.title = "Decimal to Binary"
-            self.input.text = "enter a decimal number"
-        else:
-            self.state = 0
-            self.toolbar.title = "Binary to Decimal"
-            self.input.text = "enter a binary number"
-        # hide labels until needed
-        self.converted.text = ""
-        self.label.text = ""
+    # Função para o botão Limpar
+    def limpar(self, args):
+        if self.calculado == 1:
+            self.screen.add_widget(self.input_municipio)
+            self.screen.add_widget(self.input_placas)
+            self.screen.add_widget(self.input_pot_placas)
+        self.calculado = 0
+        return 0
 
     def build(self):
-        self.state = 0 #initial state
-        #self.theme_cls.primary_palette = "DeepOrange"
-        screen = MDScreen()
 
         self.state = 0 #initial state
         self.theme_cls.primary_palette = "DeepOrange"
-        screen = MDScreen()
 
         self.toobar = MDToolbar()
         self.toobar.title = 'Calculadora Solar'
         self.toobar.pos_hint = {"top": 1}        
         self.toobar.left_action_items = [["menu", lambda x: self.menu(x)]]
         # self.toobar.right_action_items = [["account-circle", lambda x: self.menu_user(x)]]
-        screen.add_widget(self.toobar)
+        self.screen.add_widget(self.toobar)
         
         # Logo
-        screen.add_widget(Image(
+        self.screen.add_widget(Image(
             source="Logo_pequena.png",
             pos_hint = {"center_x":0.5,"center_y":0.8}
         ))
 
-        # Labels ====================================================
+        # Labels Form ====================================================
         self.municipio = MDLabel(
             text = "Município",
             halign="center",
-            pos_hint = {"center_x": 0.5, "center_y":0.62},
-            theme_text_color = "Primary",
+            pos_hint = {"center_x": 0.5, "center_y":0.66},
+            theme_text_color = "ContrastParentBackground",
             font_style = "H5"
         )
         self.n_placas = MDLabel(
             text = "Número de Placas",
             halign="center",
-            pos_hint = {"center_x": 0.5, "center_y":0.5},
-            theme_text_color = "Primary",
+            pos_hint = {"center_x": 0.5, "center_y":0.54},
+            theme_text_color = "ContrastParentBackground",
             font_style = "H5"
         )
         self.pot_placas = MDLabel(
             text = "Potência da Placa",
             halign="center",
-            pos_hint = {"center_x": 0.5, "center_y":0.38},
-            theme_text_color = "Primary",
+            pos_hint = {"center_x": 0.5, "center_y":0.42},
+            theme_text_color = "ContrastParentBackground",
             font_style = "H5"
         )
-        self.detalhes = MDLabel(
-            halign="center",
-            pos_hint = {"center_x": 0.9, "center_y":0.56},
-            theme_text_color = "Secondary"
-        )
-        screen.add_widget(self.municipio)
-        screen.add_widget(self.n_placas)
-        screen.add_widget(self.pot_placas)
         
+        self.pot_sist = MDLabel(
+            text = "Potência: 5,0 kWp",
+            halign="center",
+            pos_hint = {"center_x": 0.5, "center_y":0.25},
+            theme_text_color = "ContrastParentBackground",
+            font_style = "H5"
+        )
+        self.ger_mensal = MDLabel(
+            text = "Geração Mensal: 545 kWh",
+            halign="center",
+            pos_hint = {"center_x": 0.5, "center_y":0.20},
+            theme_text_color = "ContrastParentBackground",
+            font_style = "H5"
+        )
+        
+        self.screen.add_widget(self.municipio)
+        self.screen.add_widget(self.n_placas)
+        self.screen.add_widget(self.pot_placas)
+        self.screen.add_widget(self.ger_mensal)
+        self.screen.add_widget(self.pot_sist)
+        
+        # Labels Answers ====================================================
 
-        # Inputs =======================================================================
+
+
+        # Inputs ========================================================
         self.input_municipio = MDTextField(
             text="",
             halign="center",
             size_hint = (0.8,1),
-            pos_hint = {"center_x": 0.5, "center_y":0.56},
+            pos_hint = {"center_x": 0.5, "center_y":0.60},
             font_size = 22
         )
         self.input_placas = MDTextField(
             text="",
             halign="center",
             size_hint = (0.8,1),
-            pos_hint = {"center_x": 0.5, "center_y":0.44},
+            pos_hint = {"center_x": 0.5, "center_y":0.48},
             font_size = 22
         )
         self.input_pot_placas = MDTextField(
             text="",
             halign="center",
             size_hint = (0.8,1),
-            pos_hint = {"center_x": 0.5, "center_y":0.32},
+            pos_hint = {"center_x": 0.5, "center_y":0.36},
             font_size = 22
         )
-        screen.add_widget(self.input_municipio)
-        screen.add_widget(self.input_placas)
-        screen.add_widget(self.input_pot_placas)
+        self.screen.add_widget(self.input_municipio)
+        self.screen.add_widget(self.input_placas)
+        self.screen.add_widget(self.input_pot_placas)
         
         # "CONVERT" button=============================================================
-        screen.add_widget(MDFillRoundFlatButton(
+        self.screen.add_widget(MDFillRoundFlatButton(
             text="Calcular",
             font_size = 17,
-            pos_hint = {"center_x": 0.5, "center_y":0.1},
+            pos_hint = {"center_x": 0.4, "center_y":0.1},
             on_press = self.calcular
         ))
+        self.screen.add_widget(MDFillRoundFlatButton(
+            text="Limpar",
+            font_size = 17,
+            pos_hint = {"center_x": 0.6, "center_y":0.1},
+            on_press = self.limpar
+        ))
 
-        return screen
+        return self.screen
 
     
 if __name__ == '__main__':
